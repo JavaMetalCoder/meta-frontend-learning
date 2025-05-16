5dff9f91c0bdeea1eb2ba103f32aaa990cba2150
Fabio <zagariafabio1999@gmail.com>
2025-05-16 06:50:29 +0200

    feat: Update es1.js

--- corsi/html, css, js/es2/es1.js
+++ corsi/html, css, js/es2/es1.js
@@ -4,3 +4,22 @@ for (let i = 1; i <= 4; i++) {
     console.log("Giorno: ", j);
   }
 }
+
+let genres = [];
+
+genres.push("Metalcore");
+genres.push("Dubstep");
+genres.push("Classic");
+genres.pop(); 
+
+
+let me = {
+  age : 26,
+  isWorking : true,
+  saluta : function() {
+               console.log("Hiii");
+}
+};
+me.name = "Fabio";
+
+console.log(me," and ", genres);
